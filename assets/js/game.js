8ca51ce90598ded005d0118bfbca2326922386a3
koiclone <cheryljcruz1@gmail.com>
2022-02-23 12:55:32 -0600

    bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,27 +7,60 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+//function to check if player wants to fight or skip
+var fightOrSkip = function() {
+  //ask player if they'd like to fight or skip using fightOrSkip function
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+
+  //Conditional recursive function call
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  //convert promptFight to all lowercase so we can check with less options
+  promptFight = promptFight.toLowerCase();
+
+  //if player picks "skip" confirm  and then stop the loop
+  if (promptFight === "skip") {
+    //confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    //if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerInfo.playerMoney = playerInfo.money - 10;
+      
+    //return true if players wants to leave
+    return true;
+    
+    }
+  }
+  return false;
+};
+
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
-  while (playerInfo.health > 0 && enemy.health > 0) {
-     // ask the player if they'd like to fight or run]
-     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+  // keep track of who goes first
+  var isPlayerTurn = true;
 
-     // if player picks "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      // confirm player wants to skip
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  //randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
 
-      // if yes (true), leave fight
-      if (confirmSkip) {
-        window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-        // subtract money from playerMoney for skipping
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money)
-        break;
-      }
-    }
+// repeat and execute aes long as the enemy-robot is alive
+while (playerInfo.health > 0 && enemy.health > 0) {
+  // ask player if they'd like to fight or skip
+if (fightOrSkip()) {
+  // if true, leave fight by breaking loop
+  break;
+}
+
+
 // generate random damage value based on player's attack power
    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
